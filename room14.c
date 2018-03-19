e90f771039c309e4e4e7a1ddd562c507140da1ae
Brian Baskovich <bbaskovich@nsmb208-xx.csudh.edu>
2018-03-19 16:55:33 -0700

    v1

---
+++ room14.c
@@ -0,0 +1,27 @@
+//Brian Baskovich
+//CSC251-01
+//Room Project v1
+
+#include <stdlib.h>
+#include <stdio.h>
+
+bool complete1 = false;
+char[] name;
+char[] quest;
+char[] color;
+char[] capital;
+
+int main(void)
+{
+	puts("You enter room 14 and the door closes behind you. You hear a click that tells you the door is sealed from the water outside, but also locked. The first thing you notice is that it's dark. Like really dark. Darker than I like my coffee. And that's pretty dark. Anyway, you hear a disembodied voice that seems to come from everywhere. It tells you that if you want to progress you must correctly answer all its questions."
+	puts("He asks his first question: \"WHAT... is your name?\"")
+	scanf("%s", name);
+	puts("You hear an electric hum as the lights in the room turn on and begin to brighten. They're uncomfortably bright, like looking into the sun, but at least you can see. The room is quite large - larger than it should be considering how close the other doors were. Besides the lights, the room is completely and utterly undecorated. So much so it doesnt even have a door. Including the one you came through. Strange, isn't it?");
+	puts("\"WHAT... is your quest?\"");
+	scanf("%s", quest);
+	("WHAT... is your favorite color?")
+	scanf("%s", color);
+	printf("The lights dim to a more comfortable amount and slowly turn %s.\n", color);
+	puts("WHAT... is the capital of Assyria?");
+	scanf("%s", capital);
+	
